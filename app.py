2ab1a6a727dfa7447628a6291966c46629e772a0
Jing Zheng <jing_j_zheng@yahoo.com>
2024-03-10 13:49:29 -0400

    inventory web service using flask and postgresQL

---
+++ app.py
@@ -0,0 +1,93 @@
+import datetime
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    __tablename__ = 'item'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(255))
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    date = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def json(self):
+        return {'id': self.id, 'name': self.name, 'description': self.description, 'quantity': self.quantity, 'price': self.price }
+
+db.create_all()
+
+
+# get all items
+@app.route('/items', methods=['GET'])
+def get_items():
+    try:
+        items = Item.query.all()
+        return make_response(jsonify({'items': [item.json() for item in items]}), 200)
+        #return make_response(jsonify({'message': 'no items found'}), 404)
+    except Exception as e:
+        return make_response(jsonify({'message': 'no items found'}), 500)
+
+    
+# get a user by id
+@app.route('/items/<int:id>', methods=['GET']) 
+def get_item(id):
+    try:
+        item = Item.query.filter_by(id=id).first()
+        return make_response(jsonify({'item': item.json() }), 200)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error getting item'}), 500)   
+
+
+# craete  an item
+@app.route('/item', methods=['POST'])
+def craete_user():
+    try:
+        data = request.get_json()
+        print("data is " + format(data))
+       # if data is not None:
+       #     return make_response(jsonify({'name': format(data)}), 200)
+       # else:
+       #     return make_response(jsonify({'message': 'data not found'}), 400)
+
+        new_item = Item(name=data['name'], description=data['description'], quantity=data['quantity'], price=data['price'])
+        db.session.add(new_item)
+        db.session.commit()
+        return make_response(jsonify({'message': 'item created'}), 201)
+    except Exception as e:
+        return make_response(jsonify({'message', 'error createing item'}), 500)
+
+
+# update an item
+@app.route('/items/<int:id>', methods=['PUT'])
+def update_user(id):
+    try:
+        item = Item.query.filter_by(id=id).first()
+        if item:
+            data = request.get_json()
+            item.name = data['name']
+            item.price = data['price']
+            db.session.commit()
+            return make_response(jsonify({'message': 'item updated'}), 200)
+        return make_response(jsonify({'message': 'item not found'}), 404)
+    except Exception:
+        return make_response(jsonify({'message': 'error updating item'}), 500)
+    
+# delete an item
+def delete_user(id):
+    try:
+        item = Item.query.filter_by(id=id).first()
+        if item:
+            db.session.delete(item)
+            db.session.commit()
+            return make_response(jsonify({'message': 'item deleted'}))
+        return make_response(jsonify({'message': 'item not found'}), 404)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error updating item'}), 500)
+    
